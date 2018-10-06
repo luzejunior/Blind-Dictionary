66dd85fec500cd9008010678c3dc01b9a101e277
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2018-10-06 15:10:27 -0300

    handle json decode error and adjetivo added

--- Requests/DictionaryJson.py
+++ Requests/DictionaryJson.py
@@ -7,31 +7,51 @@ class DictionaryJson:
         self.similar_url = "http://www.dicionario-aberto.net/search-json?like="
 
     def get_definition(self, word):
-        temp_word = word
-        if word[-2:] == "ou":
-            word_list = list(word)
-            word_list[-2:] = "ar"
-            temp_word = "".join(word_list)
-        elif word[-2:] == "eu":
-            word_list = list(word)
-            word_list[-2:] = "er"
-            temp_word = "".join(word_list)
+        url_path = self.definition_url + word
+        data = self.__make_request(url_path)
 
-        url_path = self.definition_url + temp_word
-        response = requests.get(url_path)
-        data = response.json()
-        if 'superEntry' in data:
-            classification = self.__get_classification(data['superEntry'][0]['entry']['sense'][0]['gramGrp'])
+        if data:
+            if 'superEntry' in data:
+                classification = self.__get_classification(data['superEntry'][0]['entry']['sense'][0]['gramGrp'])
+            elif 'entry' in data:
+                classification = self.__get_classification(data['entry']['sense'][0]['gramGrp'])
         else:
-            classification = self.__get_classification(data['entry']['sense'][0]['gramGrp'])
+            if word[-2:] == "ou":
+                word_list = list(word)
+                word_list[-2:] = "ar"
+                temp_word = "".join(word_list)
+            elif word[-4:] == "ando":
+                word_list = list(word)
+                word_list[-4:] = "ar"
+                temp_word = "".join(word_list)
+            elif word[-2:] == "eu":
+                word_list = list(word)
+                word_list[-2:] = "er"
+                temp_word = "".join(word_list)
+            elif word[-4:] == "endo":
+                word_list = list(word)
+                word_list[-4:] = "er"
+                temp_word = "".join(word_list)
+            recursive = self.get_definition(temp_word)
+            classification = recursive[1]
+
         return [word, classification]
+
+    def __make_request(self, url):
+        response = requests.get(url)
+        try:
+            return json.loads(response.text)
+        except json.JSONDecodeError:
+            return False
 
     def __get_classification(self, response_class):
         if response_class == "f.":
             return "Substantivo Feminino"
         elif response_class == "m.":
             return "Substantivo Masculino"
-        elif response_class == "v. t." or response_class == "v. i." or response_class == "v." :
+        elif "v." in response_class:
             return "Verbo"
-        elif response_class == "pron.":
+        elif "pron." in response_class:
             return "Pronome"
+        elif "adj." in response_class:
+            return "Adjetivo"
