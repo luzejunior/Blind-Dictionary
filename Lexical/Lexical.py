834f596aacab74e7ee3edf16e1eec222237ef467
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2018-10-20 11:23:51 -0300

    english oxford dictionary added

--- Lexical/Lexical.py
+++ Lexical/Lexical.py
@@ -6,9 +6,6 @@ class Lexical:
         self.word_counter = 1
         self.dictionary = []
         self.dictionaryJson = DictionaryJson()
-        self.artigos = ["a", "o", "as", "os", "um", "uma", "uns", "umas"]
-        self.preposicao = ["ao","aos","do","dos","da","das","no","nos","na","nas","nele","neles","nela","nelas","dali","dele","deles","dela","delas","à","às","àqueles",
-        "àquelas","àquilo","de","como","para","em","por","sem","com"]
 
     def analize_phrase(self, phrase):
         self.dictionary = []
@@ -19,12 +16,5 @@ class Lexical:
             self.word_counter = self.word_counter + 1
 
     def __analize_token(self, token):
-        if token in self.artigos:
-            self.dictionary.append([token, "Artigo", self.word_counter])
-        elif token in self.preposicao:
-            self.dictionary.append([token, "Preposicao", self.word_counter])
-        elif token[0].isupper():
-            self.dictionary.append([token, "Substantivo", self.word_counter])
-        else:
-            result = self.dictionaryJson.get_definition(token)
-            self.dictionary.append([result[0], result[1], self.word_counter])
+        result = self.dictionaryJson.get_definition(token)
+        self.dictionary.append([result[0], result[1], result[2], self.word_counter])
--- Requests/DictionaryJson.py
+++ Requests/DictionaryJson.py
@@ -3,85 +3,56 @@ import requests
 
 class DictionaryJson:
     def __init__(self):
-        self.definition_url = "http://www.dicionario-aberto.net/search-json/"
-        self.similar_url = "http://www.dicionario-aberto.net/search-json?like="
+        self.app_id = '01993288'
+        self.app_key = '54200225d71891a07577574d80ac570c'
+        self.language = 'en'
+        self.definition_url = 'https://od-api.oxforddictionaries.com:443/api/v1/inflections/'  + self.language + '/'
+        self.spelling_mistake_url = 'https://languagetool.org/api/v2/check?language=en-US&text='
 
     def get_definition(self, word, recursive = False):
-        url_path = self.definition_url + word
+        url_path = self.definition_url + word.lower()
         data = self.__make_request(url_path)
 
         if data:
-            if 'superEntry' in data:
-                classification = self.__get_classification(data['superEntry'][0]['entry']['sense'][0]['gramGrp'])
-            elif 'entry' in data:
-                classification = self.__get_classification(data['entry']['sense'][0]['gramGrp'])
-        elif recursive:
-            new_word = self.__search_similar_words(word)
-            result = self.get_definition(new_word)
-            return [result[0], result[1], True]
-        else:
-            temp_word = word
-            if word[-2:] == "ou":
-                word_list = list(word)
-                word_list[-2:] = "ar"
-                temp_word = "".join(word_list)
-            elif word[-4:] == "ando":
-                word_list = list(word)
-                word_list[-4:] = "ar"
-                temp_word = "".join(word_list)
-            elif word[-2:] == "eu":
-                word_list = list(word)
-                word_list[-2:] = "er"
-                temp_word = "".join(word_list)
-            elif word[-4:] == "endo":
-                word_list = list(word)
-                word_list[-4:] = "er"
-                temp_word = "".join(word_list)
-            elif word[-2:] == "iu":
-                word_list = list(word)
-                word_list[-2:] = "ir"
-                temp_word = "".join(word_list)
-            elif word[-4:] == "indo":
-                word_list = list(word)
-                word_list[-4:] = "ir"
-                temp_word = "".join(word_list)
-            recursive = self.get_definition(temp_word, True)
-            if recursive[2]:
-                word = recursive[0]
-            classification = recursive[1]
+            if 'results' in data:
 
-        return [word, classification, False]
+                for i in range(0,len(data['results'][0]['lexicalEntries'])):
+                    #print (i)
+                    found = False
+                    if 'grammaticalFeatures' in data['results'][0]['lexicalEntries'][i]:
+                        feature = data['results'][0]['lexicalEntries'][i]['grammaticalFeatures'][0]['text']
+                        classification = data['results'][0]['lexicalEntries'][i]['lexicalCategory']
+                        break
+
+                    if not found:
+                        feature = ""
+                        classification = data['results'][0]['lexicalEntries'][i]['lexicalCategory']
+                #else:
+                #    feature = data['results'][0]['lexicalEntries'][1]['grammaticalFeatures'][0]['text']
+                #    classification = data['results'][0]['lexicalEntries'][1]['lexicalCategory']
+        else:
+            temp_word = self.__search_similar_words(word)
+            return self.get_definition(temp_word)
+
+        return [word, classification, feature]
 
     def __make_request(self, url):
-        response = requests.get(url)
+        response = requests.get(url, headers = {'app_id' : self.app_id, 'app_key' : self.app_key})
+        #print(response.text)
         try:
             return json.loads(response.text)
         except json.JSONDecodeError:
             return False
 
     def __search_similar_words(self, word):
-        print("A palavra: " + word + " está errada")
-        url_path = self.similar_url + word
+        print("We found a spelling mistake in word: " + word)
+        url_path = self.spelling_mistake_url + word
         data = self.__make_request(url_path)
-
         if data:
             counter = 1
-            print("Você quis dizer: ")
-            for token in data["list"]:
-                print(str(counter) + " - " + token)
+            print("Did you mean: ")
+            for token in data['matches'][0]['replacements']:
+                print(str(counter) + " - " + token['value'])
                 counter = counter + 1
-            selection = input("Escolha um número: ")
-            return data["list"][int(selection) - 1]
-
-
-    def __get_classification(self, response_class):
-        if response_class == "f.":
-            return "Substantivo Feminino"
-        elif response_class == "m.":
-            return "Substantivo Masculino"
-        elif "v." in response_class:
-            return "Verbo"
-        elif "pron." in response_class:
-            return "Pronome"
-        elif "adj." in response_class:
-            return "Adjetivo"
+            selection = input("Choose a number: ")
+            return data['matches'][0]['replacements'][int(selection) - 1]['value']
